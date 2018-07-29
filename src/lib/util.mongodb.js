d2831b07fb2d305d5e6d1a90ea7a8214fdfa3868
rickyrattlesnake <ricky.ratnayake@gmail.com>
2018-07-29 10:00:46 +1000

    fix: mlabs database name provided

--- src/lib/util.mongodb.js
+++ src/lib/util.mongodb.js
@@ -2,48 +2,45 @@ const MongoClient = require('mongodb').MongoClient;
 const MongoLogger = require('mongodb').Logger;
 MongoLogger.setLevel('info');
 
-const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
-const dbName = 'taxTracker';
+const defaultDbName = 'taxTracker';
+const connectionUrl = process.env.MONGODB_URI || `mongodb://localhost:27017/${defaultDbName}`;
 const profileCollectionName = 'profiles';
 const userCollectionName = 'users';
+
+console.log(`[-] util.mongodb :: connectionUrl=${connectionUrl}`);
 
 let profilesCollection;
 let usersCollection;
 const initialized = (async function() {
-  const connectionUrl = `${mongodbUri}/${dbName}`;
-  console.log('[-] accessor.js :: connectionUrl =', connectionUrl);
 
   let client;
   try {
     client = await MongoClient.connect(connectionUrl, { useNewUrlParser: true });
-
+    const db = await client.db();
 
     try {
-      profilesCollection = await client.db(dbName).createCollection(profileCollectionName);
+      profilesCollection = await db.createCollection(profileCollectionName);
       await profilesCollection.createIndex({ profileId: 1}, { unique: true });
     } catch (error) {
       if (error.name === 'MongoError' &&
           error.codeName === 'NamespaceExists') {
-        console.log(`[~] accessor.js :: collection ${profileCollectionName} already exists`);
+        console.warn(`[~] accessor.js :: collection ${profileCollectionName} already exists`);
       } else {
         throw error;
       }
     }
 
     try {
-      usersCollection = await client.db(dbName).createCollection(userCollectionName);
+      usersCollection = await db.createCollection(userCollectionName);
       await usersCollection.createIndex({ username: 1}, { unique: true });
     } catch (error) {
       if (error.name === 'MongoError' &&
       error.codeName === 'NamespaceExists') {
-        console.log(`[~] accessor.js :: collection ${userCollectionName} already exists`);
+        console.warn(`[~] accessor.js :: collection ${userCollectionName} already exists`);
       } else {
         throw error;
       }
     }
-
-
-    console.log('[v] accessor.js :: connection success');
   } catch (err) {
     console.error(err);
   }
